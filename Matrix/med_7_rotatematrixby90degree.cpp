021d98716dc9c09458ce1e187133e97419a80d3c
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-05-23 15:00:25 +0530

    Create med_7_rotatematrixby90degree.cpp

---
+++ Matrix/med_7_rotatematrixby90degree.cpp
@@ -0,0 +1,17 @@
+
+//Optimized solution
+//TC: O(n^2)
+//SC: O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+    }
+};
