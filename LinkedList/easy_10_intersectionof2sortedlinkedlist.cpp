65ca2d9154292a0d7cc14f862e209a8f2c68bd1a
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-07 20:16:04 +0530

    Create easy_10_intersectionof2sortedlinkedlist.cpp

---
+++ LinkedList/easy_10_intersectionof2sortedlinkedlist.cpp
@@ -0,0 +1,46 @@
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+//Most optimized solution
+//TC: O(m+n)
+//SC: O(m+n)
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node *head=NULL;
+    Node *temp=NULL;
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data == head2->data){
+            if(head==NULL){
+                Node *newhead=new Node(head1->data);
+                newhead->next=NULL;
+                temp=newhead;
+                head=newhead;
+            }
+            else{
+                Node *newnode=new Node(head1->data);
+                temp->next=newnode;
+                temp=newnode;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        else if(head1->data<head2->data)
+            head1=head1->next;
+        else
+            head2=head2->next;
+    }
+    return head;
+}
