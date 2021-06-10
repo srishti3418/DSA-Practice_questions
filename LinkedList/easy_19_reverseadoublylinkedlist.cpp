5d7a665f94bbf71a0a1a96e04301759044cd2193
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-11 00:15:22 +0530

    Update easy_19_reverseadoublylinkedlist.cpp

--- LinkedList/easy_19_reverseadoublylinkedlist.cpp
+++ LinkedList/easy_19_reverseadoublylinkedlist.cpp
@@ -3,20 +3,17 @@
 //SC: O(1)
 struct Node* reverseDLL(struct Node * head)
 {
-   if(head==NULL || head->next==NULL)
+    if(head==NULL || head->next==NULL)
         return head;
-    struct Node *current=head;
-    struct Node *nextnode=head->next;
-    struct Node *temp=NULL;
+    struct Node*curr=head;
+    struct Node*temp=NULL;
     
-    while(nextnode!=NULL){
-        if(current==head){
-            current->next=NULL;
-        }
-        temp=nextnode->next;
-        nextnode->next=current;
-        current=nextnode;
-        nextnode=temp;
+    while(curr!=NULL){
+        temp=curr->prev;
+        curr->prev=curr->next;
+        curr->next=temp;
+        curr=curr->prev;
     }
-    return current;
+   
+    return temp->prev;
 }
