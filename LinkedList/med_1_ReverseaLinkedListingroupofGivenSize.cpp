8791b2714d8225b87f143d5d760343def82197df
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-16 13:37:06 +0530

    optimized solution using stack

--- LinkedList/med_1_ReverseaLinkedListingroupofGivenSize.cpp
+++ LinkedList/med_1_ReverseaLinkedListingroupofGivenSize.cpp
@@ -1,25 +1,7 @@
-/*
-  Reverse a linked list
-  The input list will have least one element  
-  Return the node which points to the head of the new LinkedList
-  Node is defined as 
-    struct node
-    {
-        int data;
-        struct node* next;
-    
-        node(int x){
-            data = x;
-            next = NULL;
-        }
-    
-    }*head;
-*/
-
-//Most optimized solution (recursive approach)
+//Brute force solution (recursive approach)
 //TC: O(n)
-//SC: O(n)
-class Solution
+//SC: O(n/k)
+/*class Solution
 {
     public:
     struct node *reverse (struct node *head, int k)
@@ -41,5 +23,46 @@ class Solution
         if(nextnode!=NULL)
             head->next=reverse(nextnode,k);
         return current;
+    }
+};*/
+
+//Most optimized solution (using stack)
+//TC: O(n)
+//SC: O(k)
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        if(head==NULL || head->next==NULL)
+            return head;
+       struct node *current=head;
+       struct node *prev=NULL;
+       stack<node *> s;
+       
+       while(current!=NULL){
+           
+           int count=0;
+           while(current!=NULL && count<k){
+               s.push(current);
+               current=current->next;
+               count++;
+           }
+           while(!s.empty()){
+               if(prev==NULL){
+                   prev=s.top();
+                   head=prev;
+                   s.pop();
+               }
+               else{
+                   prev->next=s.top();
+                   prev=prev->next;
+                   s.pop();
+               }
+           }
+       }
+        prev->next=NULL;
+        return head;
     }
 };
