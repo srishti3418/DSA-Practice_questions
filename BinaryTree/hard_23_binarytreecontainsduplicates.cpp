d0a49d4751438e2013ec053acfb052dcb26982b4
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-21 16:12:18 +0530

    IMPORTANT

---
+++ BinaryTree/hard_23_binarytreecontainsduplicates.cpp
@@ -0,0 +1,63 @@
+//Most optimized solution(Using recursion and hashmap)
+//TC: O(n)
+//SC: O(n)
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+
+        node(int val){
+            data=val;
+            left=NULL;
+            right=NULL;
+        }
+};
+
+string checkduplicates(node *root, unordered_map<string, int> &m){
+    string s="";
+    if(!root)
+        return '$';
+    if(!root->left && !root->right){
+        s=to_string(root->data);
+        return s;
+    }
+    s = s+to_string(root->data);
+    s = s+checkduplicates(root->left);
+    s = s+checkduplicates(root->right);
+
+    return s;
+
+}
+
+bool checkduplicate_trees(node *root){
+    if(root==NULL)
+        return false;
+    unordered_map<string, int> m;
+    checkduplicates(root, m);
+    for(auto x:m){
+        if(x.second>=2)
+            return true;
+    }
+    return false;
+    
+}
+
+int main(){
+    node *root=new node(1);
+    root->left=new node(-2);
+    root->right=new node(3);
+    root->left->left=new node(4);
+    root->left->right=new node(5);
+    root->right->left=new node(-6);
+    root->right->right=new node(2);
+    
+    cout<<checkduplicate_trees(root)<<endl;
+    
+    
+}
