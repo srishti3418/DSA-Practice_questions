e22cd88d5baed65bddb0153852c5745c9f684c7c
srishti3418 <76532197+srishti3418@users.noreply.github.com>
2021-06-28 19:20:35 +0530

    Update med_1_deletenode.cpp

--- BST/med_1_deletenode.cpp
+++ BST/med_1_deletenode.cpp
@@ -50,3 +50,50 @@ public:
     }
 };
 
+
+//optimized solution
+//TC: O(n). {Once check}
+//SC: O(1)
+private TreeNode deleteRootNode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null) {
+            return root.right;
+        }
+        if (root.right == null) {
+            return root.left;
+        }
+        TreeNode next = root.right;
+        TreeNode pre = null;
+        for(; next.left != null; pre = next, next = next.left);
+        next.left = root.left;
+        if(root.right != next) {
+            pre.left = next.right;
+            next.right = root.right;
+        }
+        return next;
+    }
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while(cur != null && cur.val != key) {
+            pre = cur;
+            if (key < cur.val) {
+                cur = cur.left;
+            } else if (key > cur.val) {
+                cur = cur.right;
+            }
+        }
+        if (pre == null) {
+            return deleteRootNode(cur);
+        }
+        if (pre.left == cur) {
+            pre.left = deleteRootNode(cur);
+        } else {
+            pre.right = deleteRootNode(cur);
+        }
+        return root;
+    }
+
