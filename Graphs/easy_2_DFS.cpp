0f4556b68f85512fb08ef5be74f90bece7d20cdb
srishti3418 <srishti2982@gmail.com>
2021-07-04 17:11:55 +0530

    DFS Traversal update

--- Graphs/easy_2_DFS.cpp
+++ Graphs/easy_2_DFS.cpp
@@ -1,22 +1,29 @@
+//Most optimized solution
 //TC: O(V+E)
 //SC: O(V)
-vector<int> DFS(vector<int> &result, int V, vector<int> adj[], int start, vector<int> &visited){
+class Solution
+{
+    public:
+    //Function to return Breadth First Traversal of given graph.
+    vector<int> DFS(vector<int> &result, int V, vector<int> adj[], int start, vector<int> &visited){
     
-    result.push_back(start);
-    visited[start]=1;
-
-    for(auto i:adj[start]){
-        if(!visited[i]) DFS(result, V, adj, i, visited);
-    }
-    return result;
-}
-
-vector<int> DFS(int V, vector<int> adj[]){
+        result.push_back(start);
+        visited[start]=1;
     
-    vector<int> result;
-    visited<int> visited(V, 0);
-    for(auto i: adj){
-        if(!visited[i]) return DFS(result, V, adj, 0, visited);
+        for(auto i:adj[start]){
+            if(!visited[i]) DFS(result, V, adj, i, visited);
+        }
+        return result;
     }
-
-}
+    
+	vector<int>dfsOfGraph(int V, vector<int> adj[])
+	{
+	    // Code here
+	    vector<int> result;
+        vector<int> visited(V, 0);
+        for(int i=0; i<V; i++){
+            if(!visited[i]) DFS(result, V, adj, i, visited);
+        }
+        return result;
+    }
+};
