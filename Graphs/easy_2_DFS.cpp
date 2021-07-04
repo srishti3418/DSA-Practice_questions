cd2071e194744f149375f5a1220a2369fb6d93e0
srishti3418 <srishti2982@gmail.com>
2021-07-04 16:57:59 +0530

    DFS Traversal

---
+++ Graphs/easy_2_DFS.cpp
@@ -0,0 +1,20 @@
+vector<int> DFS(vector<int> &result, int V, vector<int> adj[], int start, vector<int> &visited){
+    
+    result.push_back(start);
+    visited[start]=1;
+
+    for(auto i:adj[start]){
+        if(!visited[i]) DFS(result, V, adj, i, visited);
+    }
+    return result;
+}
+
+vector<int> DFS(int V, vector<int> adj[]){
+    
+    vector<int> result;
+    visited<int> visited(V, 0);
+    for(auto i: adj){
+        if(!visited[i]) return DFS(result, V, adj, 0, visited);
+    }
+
+}
