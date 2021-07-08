08bf61c17d2fb657aa230627e4ff4c017935754d
srishti3418 <srishti.suman35@gmail.com>
2021-07-08 14:57:21 +0530

    adding complexity

--- Graphs/hard_10_wordLadder.cpp
+++ Graphs/hard_10_wordLadder.cpp
@@ -1,48 +1,55 @@
-//TC: 
-class Solution {
+//TC: O(M*N) where M is the num of words and N is the length of the words
+class Solution
+{
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+
         unordered_set<string> myset;
-        bool isPresent=false;
-        
-        for(int i=0; i<wordList.size(); i++){
-            if(endWord.compare(wordList[i])==0)
-                isPresent=true;
+        bool isPresent = false;
+
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (endWord.compare(wordList[i]) == 0)
+                isPresent = true;
             myset.insert(wordList[i]);
         }
-        
-        if(isPresent==false)
+
+        if (isPresent == false)
             return 0;
-        
+
         queue<string> q;
         q.push(beginWord);
-        int depth=0;
-        
-        while(!q.empty()){
-            
+        int depth = 0;
+
+        while (!q.empty())
+        {
+
             depth += 1;
-            int lsize=q.size();
-            while(lsize--){
-                
-                string curr=q.front();
+            int lsize = q.size();
+            while (lsize--)
+            {
+
+                string curr = q.front();
                 q.pop();
-                
-                for(int i=0; i<curr.length(); i++){
-                     string temp=curr;
-                     for(char c='a'; c<='z'; c++){
-                         temp[i]=c;
-                         if(temp.compare(curr)==0)
-                             continue;
-                         if(temp.compare(endWord)==0)
-                             return depth+1;
-                         if(myset.find(temp)!=myset.end()){
-                             q.push(temp);
-                             myset.erase(temp);
-                         }
-                     }
+
+                for (int i = 0; i < curr.length(); i++)
+                {
+                    string temp = curr;
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
+                        temp[i] = c;
+                        if (temp.compare(curr) == 0)
+                            continue;
+                        if (temp.compare(endWord) == 0)
+                            return depth + 1;
+                        if (myset.find(temp) != myset.end())
+                        {
+                            q.push(temp);
+                            myset.erase(temp);
+                        }
+                    }
                 }
-                
             }
         }
         return 0;
