7d46c904035658b719224bd227d13f306c44e217
srishti3418 <srishti.suman35@gmail.com>
2021-07-08 14:18:37 +0530

    cleaning code

--- Graphs/med_6_minimumKnightMoves.cpp
+++ Graphs/med_6_minimumKnightMoves.cpp
@@ -1,71 +1,50 @@
 
 //TC: O(N^2)
 //SC :O(N^2)
-class Solution 
+class Solution
 {
-    public:
-    //Function to find out minimum steps Knight needs to reach target position.
-	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int n)
+public:
+	//Function to find out minimum steps Knight needs to reach target position.
+	bool isValid(int n, int x, int y)
 	{
-	    // Code here
-	    int x1=KnightPos[0];
-	    int y1=KnightPos[1];
-	    int x2=TargetPos[0];
-	    int y2=TargetPos[1];
-	    
-	    if(x1==y1 && x2==y2)
-	        return 0;
-	    
-	    int a[n][n];
-	    for(int i=0; i<n; i++){
-	        for(int j=0; j<n; j++){
-	            a[i][j]=0;
-	        }
-	    }
-	    queue<pair<int, int>> q;
-	    q.push(make_pair(x1-1, y1-1));
-	    while(!q.empty()){
-	        auto curr=q.front();
-	        int x=curr.first;
-	        int y=curr.second;
-	        
-	        if(x-2>=0 && x-2<n && y+1>=0 && y+1<n && a[x-2][y+1]==0){
-	            a[x-2][y+1]=a[x][y]+1;
-	            q.push(make_pair(x-2, y+1));
-	        }
-	        if(x-1>=0 && x-1<n && y+2>=0 && y+2<n && a[x-1][y+2]==0){
-	            a[x-1][y+2]=a[x][y]+1;
-	            q.push(make_pair(x-1, y+2));
-	        }
-	        if(x+1>=0 && x+1<n && y+2>=0 && y+2<n && a[x+1][y+2]==0){
-	            a[x+1][y+2]=a[x][y]+1;
-	            q.push(make_pair(x+1, y+2));
-	        }
-	        if(x+2>=0 && x+2<n && y+1>=0 && y+1<n && a[x+2][y+1]==0){
-	            a[x+2][y+1]=a[x][y]+1;
-	            q.push(make_pair(x+2, y+1));
-	        }
-	        if(x+2>=0 && x+2<n && y-1>=0 && y-1<n && a[x+2][y-1]==0){
-	            a[x+2][y-1]=a[x][y]+1;
-	            q.push(make_pair(x+2, y-1));
-	        }
-	        if(x+1>=0 && x+1<n && y-2>=0 && y-2<n && a[x+1][y-2]==0){
-	            a[x+1][y-2]=a[x][y]+1;
-	            q.push(make_pair(x+1, y-2));
-	        }
-	        if(x-1>=0 && x-1<n && y-2>=0 && y-2<n && a[x-1][y-2]==0){
-	            a[x-1][y-2]=a[x][y]+1;
-	            q.push(make_pair(x-1, y-2));
-	        }
-	         if(x-2>=0 && x-2<n && y-1>=0 && y-1<n && a[x-2][y-1]==0){
-	            a[x-2][y-1]=a[x][y]+1;
-	            q.push(make_pair(x-2, y-1));
-	        }
-	        
-	        q.pop();
-	    }
-	    return a[x2-1][y2-1];
+		return (x >= 0 && x < n && y >= 0 && y < n);
 	}
-	
-	
+
+	int minStepToReachTarget(vector<int> &KnightPos, vector<int> &TargetPos, int n)
+	{
+		// Code here
+		int x1 = KnightPos[0];
+		int y1 = KnightPos[1];
+		int x2 = TargetPos[0];
+		int y2 = TargetPos[1];
+
+		if (x1 == y1 && x2 == y2)
+			return 0;
+
+		vector<vector<int>> a(n, vector<int>(n, 0));
+
+		int dx[] = {2, 1, -1, -2, -2, -1, 1, 2};
+		int dy[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+		queue<pair<int, int>> q;
+		q.push(make_pair(x1 - 1, y1 - 1));
+		while (!q.empty())
+		{
+			auto curr = q.front();
+			int x = curr.first;
+			int y = curr.second;
+			if (x == x2 - 1 && y == y2 - 1)
+				break;
+			for (int k = 0; k < 8; k++)
+			{
+				if (isValid(n, x + dx[k], y + dy[k]) && a[x + dx[k]][y + dy[k]] == 0)
+				{
+					a[x + dx[k]][y + dy[k]] = a[x][y] + 1;
+					q.push(make_pair(x + dx[k], y + dy[k]));
+				}
+			}
+			q.pop();
+		}
+		return a[x2 - 1][y2 - 1];
+	}
 };
