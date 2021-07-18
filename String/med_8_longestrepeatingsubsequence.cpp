0aeb44b9dcb1aa4ff192cbbfc4534d7ea005c138
Srishti Suman <76532197+srishti3418@users.noreply.github.com>
2021-07-18 21:10:25 +0530

    Update med_8_longestrepeatingsubsequence.cpp

--- String/med_8_longestrepeatingsubsequence.cpp
+++ String/med_8_longestrepeatingsubsequence.cpp
@@ -3,15 +3,15 @@ class Solution {
 		int LongestRepeatingSubsequence(string str){
 		    // Code here
 		    int n=str.length();
-		    int dp[n+1][n+1];
+		    int dp[2][n+1];
 		    for(int i=0;i<=n;i++){
 		        for(int j=0;j<=n;j++){
 		            if(i==0 || j==0)
-		                dp[i][j]=0;
+		                dp[i%2][j]=0;
 		            else if(str[i-1]==str[j-1] && i!=j)
-		                dp[i][j]=dp[i-1][j-1]+1;
+		                dp[i%2][j]=dp[(i-1)%2][j-1]+1;
 		            else
-		                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		                dp[i%2][j]=max(dp[(i-1)%2][j],dp[i%2][j-1]);
 		        }
 		    }
 		    return dp[n][n];
