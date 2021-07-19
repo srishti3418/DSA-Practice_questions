f092f30fb08b9ef8713437804ea5b42cefd3bcdf
srishti3418 <srishti.suman35@gmail.com>
2021-07-19 21:00:10 +0530

    similar to fibonacci

---
+++ DP/easy_extra_minCostClimbingStairs.cpp
@@ -0,0 +1,24 @@
+//Most optimized solution
+//TC: O(n)
+//SC: O(n)
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n + 1);
+        dp[0] = cost[0];
+        if (n == 0)
+            return dp[n];
+        dp[1] = cost[1];
+        for (int i = 2; i <= n; i++)
+        {
+            if (i == n)
+                dp[i] = min(dp[i - 1], dp[i - 2]);
+            else
+                dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return dp[n];
+    }
+};
