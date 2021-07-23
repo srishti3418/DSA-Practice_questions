eb89a2e93c7f568857bb12ca67fc2f2224a9aa88
srishti3418 <srishti.suman35@gmail.com>
2021-07-23 13:57:58 +0530

    implementation using array

---
+++ stackAndQueue/stackimplementUsingArray.cpp
@@ -0,0 +1,69 @@
+//TC: O(1) for all operations except printing which will take O(n)
+//SC: O(n)
+#include <iostream>
+using namespace std;
+
+class stack
+{
+    int *arr;
+    int i;
+    int n;
+
+public:
+    stack(int size)
+    {
+        n = size;
+        arr = new int[size];
+        i = 0;
+    }
+    void push(int val)
+    {
+        if (i == n)
+        {
+            cout << "stack overflow" << endl;
+            return;
+        }
+        arr[i++] = val;
+        return;
+    }
+    void pop()
+    {
+        i--;
+    }
+    bool isEmpty()
+    {
+        return i == 0;
+    }
+    int size()
+    {
+        return i;
+    }
+    void print()
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    stack s(n);
+    s.push(3);
+    if (s.isEmpty())
+        cout << "stack is empty" << endl;
+    s.push(4);
+    s.push(1);
+    s.push(6);
+    s.push(10);
+    cout << "Initial size " << s.size() << endl;
+    s.pop();
+    s.pop();
+    s.push(8);
+    cout << "Final size " << s.size() << endl;
+    s.print();
+}
