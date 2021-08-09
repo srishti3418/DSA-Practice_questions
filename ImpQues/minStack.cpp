a4173556358c88e57e080b67d908baa4b2285a59
srishti3418 <srishti.suman35@gmail.com>
2021-08-09 11:25:32 +0530

    nice concept, using only 2 stacks

---
+++ ImpQues/minStack.cpp
@@ -0,0 +1,46 @@
+//Short and sweet solution
+//SC: O(n)
+//TC: O(1)
+class MinStack
+{
+public:
+    /** initialize your data structure here. */
+    stack<int> s1;
+    stack<int> s2;
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+        s1.push(val);
+        if (s2.empty() || val <= s2.top())
+            s2.push(val);
+    }
+
+    void pop()
+    {
+        if (s1.top() == s2.top())
+            s2.pop();
+        s1.pop();
+    }
+
+    int top()
+    {
+        return s1.top();
+    }
+
+    int getMin()
+    {
+        return s2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
