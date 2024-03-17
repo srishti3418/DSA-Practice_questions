809500ccc348d6bb39121c54cc8f0da44e815a78
Srishti Suman <srishti.suman@gupshup.io>
2024-03-17 20:08:06 +0530

    good optimized solution

---
+++ 40_imp_interview_question/balancedBinaryTree.cpp
@@ -0,0 +1,31 @@
+// Optmized solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int heightofBInaryTree (TreeNode *root){
+        if(root==NULL)
+            return 0;
+        int left = heightofBInaryTree(root->left);
+        if(left == -1) return -1;
+        int right = heightofBInaryTree(root->right);
+        if(right == -1) return -1;
+
+        if(abs(left-right)>1) return -1;
+        return 1+ max (left, right);
+    }
+    bool isBalanced(TreeNode* root) {
+        return heightofBInaryTree(root)!=-1;
+    }
+};
